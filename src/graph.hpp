3b1c2baafef4cdbb1f4f6123dea035d472898e85
Cirus Thenter <cirusthenter@gmail.com>
2021-04-13 01:27:45 +0900

    compute modularity given community partition

--- src/graph.hpp
+++ src/graph.hpp
@@ -4,7 +4,6 @@ class Graph {
 private:
     typedef pair<int, int> edge;
     unordered_map<int, unordered_set<int>> neighbors;
-    int array_size;
     int num_edges;
 
     void add_vertex(int v)
@@ -15,6 +14,11 @@ private:
     }
 
 public:
+    Graph()
+    {
+        num_edges = 0;
+    }
+
     // getter
     bool has_vertex(int v)
     {
@@ -46,11 +50,42 @@ public:
         }
     }
 
+    bool has_neighbor(int u, int v)
+    {
+        return neighbors[u].find(v) != neighbors[u].end();
+    }
+
+    double compute_modularity(vector<vector<int>> communities)
+    {
+        double modularity = 0;
+        for (auto community : communities) {
+            // create community set
+            unordered_set<int> nodes_in_community;
+            for (auto node : community)
+                nodes_in_community.insert(node);
+
+            int num_edges_within_community = 0;
+            int num_edges_attached_to_community = 0;
+
+            for (auto node : community) {
+                for (auto neighbor : get_neighbors(node))
+                    if (nodes_in_community.find(neighbor) != nodes_in_community.end())
+                        ++num_edges_within_community;
+                num_edges_attached_to_community += get_degree(node);
+            }
+            modularity += (double)num_edges_within_community / (2 * num_edges);
+            modularity -= (double)num_edges_attached_to_community * num_edges_attached_to_community / (4 * num_edges * num_edges);
+        }
+        return modularity;
+    }
+
     // setter
     void add_edge(int u, int v)
     {
         add_vertex(u);
         add_vertex(v);
+        if (!has_neighbor(u, v) and !has_neighbor(v, u))
+            ++num_edges;
         neighbors[u].insert(v);
         neighbors[v].insert(u);
     }
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@
 int main(int argc, char* argv[])
 {
     Graph graph = read_graph(argv[1]);
-    graph.print_graph();
+    vector<vector<int>> communities {
+        { 0, 1, 2, 3 },
+        { 4, 5, 6 },
+        { 7, 8, 9 }
+    };
+    cout << "modularity: " << graph.compute_modularity(communities) << endl;
+
     return 0;
 }
